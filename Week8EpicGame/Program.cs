fdd5d307c6cb271820c74c2a4c5adbae6e5d58f5
Nele Oks <nele.oks@tktk.ee>
2024-12-10 21:35:27 +0200

    add Week8EpicGame code

---
+++ Week8EpicGame/Program.cs
@@ -0,0 +1,37 @@
+﻿string folderPath = @"C:\Users\****\Documents\data\";
+string heroFile = "heroes.txt";
+string villainFile = "villains.txt";
+
+string[] heroes = File.ReadAllLines(Path.Combine(folderPath, heroFile));
+string[] villains = File.ReadAllLines(Path.Combine(folderPath, villainFile));
+
+//string[] heroes = { "Harry Potter", "Elsa", "Rapunzel", "Anna" };
+//string[] villains = { "Voldemort", "Cruella", "Dracula", "Joker", "Dexter" };
+
+
+string[] weapon = { "magic wand", "plastic fork", "banana", "wooden sword", "Lego brick" };
+
+//Random rnd = new Random(); - lisasin static koodi
+//int randomIndex = rnd.Next(0, heros.Length);
+
+//string hero = heros[randomIndex]; - all muudetud
+string hero = GetRandomValueFromArray(heroes);
+string heroWeapon = GetRandomValueFromArray(weapon);
+Console.WriteLine($"Today {hero} with {heroWeapon} saves the day");
+
+
+
+//randomIndex = rnd.Next(0, villains.Length); - alumises koodis
+
+//string villain = villains[randomIndex]; muudetud kuju all
+string villain = GetRandomValueFromArray(villains);
+string villainWeapon = GetRandomValueFromArray(weapon);
+Console.WriteLine($"Today {villain} with {villainWeapon} tries to take over the world!");
+
+static string GetRandomValueFromArray(string[] someArray)
+{
+    Random rnd = new Random();
+    int randomIndex = rnd.Next(0, someArray.Length);
+    string randomStringFromArray = someArray[randomIndex];
+    return randomStringFromArray;
+}
